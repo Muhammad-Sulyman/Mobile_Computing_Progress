c15413c9342fd08ee2c819bdf03b28abe91c6f81
M Suleman Amjad <salmanamjad60@gmail.com>
2021-04-08 02:21:52 +0500

    English Alphabet App Added

---
+++ LearnEnglishAlphabets/app/src/main/java/com/example/learnenglishaplphabet/Alphabets.java
@@ -0,0 +1,154 @@
+package com.example.learnenglishaplphabet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Alphabets extends AppCompatActivity {
+
+    MediaPlayer mp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alphabets);
+
+    }
+
+
+
+    public void A(View view) {
+        mp = MediaPlayer.create(this, R.raw.a);
+        mp.start();
+    }
+
+    public void B(View view) {
+        mp = MediaPlayer.create(this, R.raw.b);
+        mp.start();
+    }
+
+    public void C(View view) {
+        mp = MediaPlayer.create(this, R.raw.c);
+        mp.start();
+    }
+
+    public void Y(View view) {
+        mp = MediaPlayer.create(this, R.raw.y);
+        mp.start();
+    }
+
+    public void X(View view) {
+        mp = MediaPlayer.create(this, R.raw.x);
+        mp.start();
+    }
+
+    public void D(View view) {
+        mp = MediaPlayer.create(this, R.raw.d);
+        mp.start();
+    }
+
+    public void E(View view) {
+        mp = MediaPlayer.create(this, R.raw.e);
+        mp.start();
+    }
+
+    public void F(View view) {
+        mp = MediaPlayer.create(this, R.raw.f);
+        mp.start();
+    }
+
+    public void G(View view) {
+        mp = MediaPlayer.create(this, R.raw.g);
+        mp.start();
+    }
+
+    public void H(View view) {
+        mp = MediaPlayer.create(this, R.raw.h);
+        mp.start();
+    }
+
+    public void I(View view) {
+        mp = MediaPlayer.create(this, R.raw.i);
+        mp.start();
+    }
+
+    public void J(View view) {
+        mp = MediaPlayer.create(this, R.raw.j);
+        mp.start();
+    }
+
+    public void K(View view) {
+        mp = MediaPlayer.create(this, R.raw.k);
+        mp.start();
+    }
+
+    public void L(View view) {
+        mp = MediaPlayer.create(this, R.raw.l);
+        mp.start();
+    }
+
+    public void M(View view) {
+        mp = MediaPlayer.create(this, R.raw.m);
+        mp.start();
+    }
+
+    public void N(View view) {
+        mp = MediaPlayer.create(this, R.raw.n);
+        mp.start();
+    }
+
+    public void O(View view) {
+        mp = MediaPlayer.create(this, R.raw.o);
+        mp.start();
+    }
+
+    public void P(View view) {
+        mp = MediaPlayer.create(this, R.raw.p);
+        mp.start();
+    }
+
+    public void Q(View view) {
+        mp = MediaPlayer.create(this, R.raw.q);
+        mp.start();
+    }
+
+    public void R(View view) {
+        mp = MediaPlayer.create(this, R.raw.r);
+        mp.start();
+    }
+
+    public void S(View view) {
+        mp = MediaPlayer.create(this, R.raw.s);
+        mp.start();
+    }
+
+    public void T(View view) {
+        mp = MediaPlayer.create(this, R.raw.t);
+        mp.start();
+    }
+
+    public void U(View view) {
+        mp = MediaPlayer.create(this, R.raw.u);
+        mp.start();
+    }
+
+    public void V(View view) {
+        mp = MediaPlayer.create(this, R.raw.v);
+        mp.start();
+    }
+
+    public void W(View view) {
+        mp = MediaPlayer.create(this, R.raw.w);
+        mp.start();
+    }
+
+    public void Z(View view) {
+        mp = MediaPlayer.create(this, R.raw.z);
+        mp.start();
+    }
+
+
+}
