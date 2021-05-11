5078177dcdfb7a8bf5bc359636530d1590353641
M Suleman Amjad <salmanamjad60@gmail.com>
2021-05-11 18:34:47 +0500

    C update

--- Calculator_Linear_Layout/app/src/main/java/com/example/calculator_linear_layout/MainActivity.java
+++ Calculator_Linear_Layout/app/src/main/java/com/example/calculator_linear_layout/MainActivity.java
@@ -3,14 +3,11 @@ package com.example.calculator_linear_layout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptEngine;
-import javax.script.ScriptException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -40,34 +37,31 @@ public class MainActivity extends AppCompatActivity {
         doubleString = "";
     }
 
-    public void equal(View view) throws ScriptException {
-        ScriptEngineManager scriptEngineManager= new ScriptEngineManager();
-        ScriptEngine engine = scriptEngineManager.getEngineByName("JavaScript");
-        Object result = engine.eval(expression);
-        textView.setText(result.toString());
+    public void equal(View view){
+
 
     }
 
     public void plus(View view) {
-        doubleString="";
-        expression.concat("+");
+        expression = expression.concat("+");
+        Log.d("expression","in there");
         textView.setText(expression);
     }
 
     public void multiply(View view) {
-        expression.concat("*");
+        expression = expression.concat("*");
         textView.setText(expression);
     }
 
     public void minus(View view) {
-        expression.concat("-");
+        expression = expression.concat("-");
         textView.setText(expression);
+        Log.d("expression",expression);
     }
 
     public void divide(View view) {
-        expression.concat("-");
+        expression = expression.concat("/");
         textView.setText(expression);
-
     }
 
     public void clear(View view) {
@@ -83,67 +77,67 @@ public class MainActivity extends AppCompatActivity {
 
     public void zero(View view) {
         doubleString.concat("0");
-        expression.concat("0");
+        expression = expression.concat("0");
         textView.setText(expression);
     }
 
     public void point(View view) {
         doubleString.concat(".");
-        expression.concat(".");
+        expression = expression.concat(".");
         textView.setText(expression);
     }
 
     public void three(View view) {
         doubleString.concat("3");
-        expression.concat("3");
+        expression = expression.concat("3");
         textView.setText(expression);
     }
 
     public void two(View view) {
-        expression.concat("2");
+        expression = expression.concat("2");
         textView.setText(expression.toCharArray(),0,expression.length());
         textView.setText(expression.toString());
     }
 
     public void one(View view) {
         doubleString.concat("1");
-        expression.concat("1");
+        expression = expression.concat("1");
         textView.setText(expression);
     }
 
     public void six(View view) {
         doubleString.concat("6");
-        expression.concat("6");
+        expression = expression.concat("6");
         textView.setText(expression);
     }
 
     public void five(View view) {
         doubleString.concat("5");
-        expression.concat("5");
+        expression = expression.concat("5");
         textView.setText(expression);
     }
 
     public void four(View view) {
         doubleString.concat("4");
-        expression.concat("4");
+        expression = expression.concat("4");
         textView.setText(expression);
     }
 
     public void nine(View view) {
         doubleString.concat("9");
-        expression.concat("9");
+        expression = expression.concat("9");
         textView.setText(expression);
     }
 
     public void eight(View view) {
         doubleString.concat("8");
-        expression.concat("8");
+        expression = expression.concat("8");
         textView.setText(expression);
     }
 
     public void seven(View view) {
         doubleString.concat("7");
-        expression.concat("7");
+        expression = expression.concat("7");
         textView.setText(expression);
     }
 }
